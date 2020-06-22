89c4626daca36b08db63158ff777cf6999ef7fad
shiradofu <43514606+shiradofu@users.noreply.github.com>
2020-06-22 22:00:34 +0900

    add options to brew install fzf

--- install.sh
+++ install.sh
@@ -58,6 +58,7 @@ while read formula; do
   [ -z "${formula}" ] && continue
   exists "${formula}" && continue
   case ${formula} in
+    fzf  ) (brew --prefix)/opt/fzf/install --no-key-bindings --completion --no-update-rc
     yarn ) brew install yarn --ignore-dependencies;;
     aws  ) brew install awscli;;
     *    ) brew install ${formula};;
