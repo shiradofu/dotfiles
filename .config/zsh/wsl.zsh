346a6a4cbbd3c2ad11a808a500d60335c4acf471
shiradofu <43514606+shiradofu@users.noreply.github.com>
2022-03-26 15:25:03 +0900

    wsl: add path

--- .config/zsh/wsl.zsh
+++ .config/zsh/wsl.zsh
@@ -4,3 +4,4 @@ alias open=wslview
 
 export PATH="$PATH:/mnt/c/Program Files/Docker/Docker/resources/bin"
 export PATH="$PATH:/mnt/c/ProgramData/DockerDesktop/version-bin"
+export PATH="$PATH:/mnt/c/Windows/System32"
