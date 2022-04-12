d657c03febcf49ba6c85eb9cf603b830584f6bd2
shiradofu <43514606+shiradofu@users.noreply.github.com>
2022-04-12 21:07:05 +0900

    :D

--- install.sh
+++ install.sh
@@ -1,128 +1,127 @@
 #!/bin/bash
 
-msg() { printf "\033[1;34m$1\033[0m\n"; }
-is_mac() { uname | grep Darwin -q; }
-is_wsl() { uname -r | grep microsoft -q; }
-exists() { type $1 > /dev/null 2>&1; }
-brew-install() { msg "\n🍺  installing $1...\n"; brew install $1; }
-source ${SCRIPT_DIR}/../.config/zsh/.zshenv
-SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 password=$1
 
-msg "🍺  installing formulae with homebrew..."
+msg() { printf "\033[1;34m$1\033[0m\n"; }
+exists() { type $1 > /dev/null 2>&1; }
+is_mac() { uname | grep Darwin -q; }
+is_wsl() { uname -r | grep microsoft -q; }
+brew-i() { msg "\n🍺  installing $1:\n"; brew install $1; }
 
-brew-install zsh
-brew-install fzf
-brew-install rg
-brew-install fd
-brew-install jq
-brew-install bat
-brew-install tmux
-brew-install starship
-brew-install nvim
-brew-install git
-brew-install git-delta
-brew-install gh
-brew-install ghq
-brew-install gitui
-brew-install tokei
-brew-install act
-brew-install awscli
-brew-install bitwarden-cli
-brew-install asdf
-brew-install watchman # for coc-tsserver
+source ${SCRIPT_DIR}/../.config/zsh/.zshenv
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
-echo "$1" | sudo -S sh -c "printf '${HOMEBREW_PREFIX}/bin/zsh\n' >> /etc/shells"
-$(brew --prefix)/opt/fzf/install --completion --no-update-rc --xdg
+brew-i zsh
+echo "$password" | sudo -S sh -c "printf '${HOMEBREW_PREFIX}/bin/zsh\n' >> /etc/shells"
 
+brew-i fzf
+${HOMEBREW_PREFIX}/opt/fzf/install --completion --no-update-rc --xdg
 
-# msg "\n🍺  brew install completed!"
+brew-i rg
+brew-i fd
+brew-i jq
+brew-i bat
+brew-i tmux
+brew-i starship
+brew-i git
+brew-i git-delta
+brew-i gh
+brew-i ghq
+brew-i gitui
+brew-i tokei
+brew-i act
+brew-i awscli
+brew-i bitwarden-cli
 
-msg "\ninstall languages...\n"
-
+#
+# Languages and Package Managers
+#
+brew-i asdf
 source ${HOMEBREW_PREFIX}/opt/asdf/asdf.sh
 
-brew-install llvm
-brew-install ccls
-
-msg "\nnodejs:\n"
-asdf plugin add nodejs  &&
-asdf install nodejs lts &&
-asdf global nodejs lts  &&
-npm install --global neovim
-brew-install yarn
-
-asdf plugin add yarn     &&
-asdf install yarn latest &&
-asdf global yarn latest
-
-msg "\npython:\n"
-asdf plugin add python     &&
-asdf install python 2.7.18 &&
-asdf shell python 2.7.18   &&
-pip install pynvim
-
-asdf install python 3.9.6  &&
-asdf shell python 3.9.6    &&
-pip install pynvim         &&
-asdf global python 3.9.6
-
-pip3 install neovim-remote &&
-asdf reshim python
+msg "\nc/c++:\n"
+brew-i gcc
+brew-i llvm
+brew-i ccls
 
 msg "\ngolang:\n"
 asdf plugin add golang     &&
 asdf install golang latest &&
 asdf global golang latest
-export GOPATH="$XDG_CACHE_HOME/go"
 go install golang.org/x/tools/cmd/goimports@latest
 
-brew-install php
-brew-install composer
+msg "\nnodejs:\n"
+asdf plugin add nodejs  &&
+asdf install nodejs lts &&
+asdf global nodejs lts  &&
+brew-i yarn
 
-# set -e
+msg "\npython:\n"
+asdf plugin add python     &&
+asdf install python 2.7.18 &&
 
-# ghq_shiradofu=$(ghq root)/github.com/shiradofu
-# mkdir -p $ghq_shiradofu
-# rsync -av ./dotfiles $ghq_shiradofu/
-# ghq_dotfiles=$ghq_shiradofu/dotfiles
+asdf install python 3.9.6  &&
+asdf global python 3.9.6
 
-# [ -f ".zshenv" ] && mv .zshenv .zshenv.$RANDOM.bak
-# ln -s $ghq_dotfiles/.config/zsh/.zshenv
-# [ -d ".config" ] && mv .config .config.$RANDOM.bak
-# ln -s $ghq_dotfiles/.config
-# [ -d "bin" ] && mv bin bin.$RANDOM.bak
-# ln -s $ghq_dotfiles/bin && hash -r
-# [ -f ".gitconfig" ] && mv .gitconfig .gitconfig.$RANDOM.bak
-# cp $ghq_dotfiles/.config/git/config.tpl $ghq_dotfiles/.config/git/config
+msg "\nphp:\n"
+brew-i php
+brew-i composer
 
-# if ! [ -f ".bashrc" ] || ! cat .bashrc | grep -xq 'source ~/.zshenv'; then
-#   echo 'source ~/.zshenv' >> .bashrc
-# fi
+#
+# Vim
+#
+brew-i nvim
+npm install --global neovim
+asdf shell python 2.7.18 && pip install pynvim
+asdf shell python 3.9.6  && pip install pynvim
+brew-i watchman # coc-tsserver
 
-# chcs iceberg
+#
+# Deploy files
+#
+set -e
 
-# if is_mac; then
-#   # brew install もこっちに持ってくる
-#   git config --global credential.helper osxkeychain
-#   defaults write com.apple.Dock autohide-delay -float 3600; killall Dock
-# fi
+ghq_shiradofu=$(ghq root)/github.com/shiradofu
+mkdir -p $ghq_shiradofu
+rsync -av ./dotfiles $ghq_shiradofu/
+ghq_dotfiles=$ghq_shiradofu/dotfiles
 
-# if is_wsl && exists wslvar; then
-#   curl -sLo /tmp/win32yank.zip \
-#     https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
-#   unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
-#   chmod +x /tmp/win32yank.exe
-#   mv /tmp/win32yank.exe ./bin
+[ -f ".zshenv" ] && mv .zshenv .zshenv.$RANDOM.bak
+ln -s $ghq_dotfiles/.config/zsh/.zshenv
+[ -d ".config" ] && mv .config .config.$RANDOM.bak
+ln -s $ghq_dotfiles/.config
+[ -d "bin" ] && mv bin bin.$RANDOM.bak
+ln -s $ghq_dotfiles/bin && hash -r
+[ -f ".gitconfig" ] && mv .gitconfig .gitconfig.$RANDOM.bak
+cp $ghq_dotfiles/.config/git/config.tpl $ghq_dotfiles/.config/git/config
 
-#   userprofile=$(wslpath $(wslvar USERPROFILE))
-#   wslsync .wslconfig
-#   wslsync wls.conf --password $password
-#   winterm-gen --colorscheme Iceberg
-#   wslsync winterm
+if ! [ -f ".bashrc" ] || ! cat .bashrc | grep -xq 'source ~/.zshenv'; then
+  echo 'source ~/.zshenv' >> .bashrc
+fi
 
-#   git config --global credential.helper \
-#     "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe"
-# fi
+#
+# OS-spesific settings
+#
+if is_mac; then
+  git config --global credential.helper osxkeychain
+  defaults write com.apple.Dock autohide-delay -float 3600; killall Dock
+fi
 
-# unset $password
+if is_wsl && exists wslvar; then
+  curl -sLo /tmp/win32yank.zip \
+    https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+  unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+  chmod +x /tmp/win32yank.exe
+  mv /tmp/win32yank.exe ./bin
+
+  userprofile=$(wslpath $(wslvar USERPROFILE))
+  wslsync .wslconfig
+  wslsync wls.conf --password $password
+  winterm-gen --colorscheme Iceberg
+  wslsync winterm
+
+  git config --global credential.helper \
+    "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe"
+fi
+
+unset $password
